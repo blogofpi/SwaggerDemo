ced96b9c113607d409b64501bc025d7600e114e3
Senthil <blogofpi@gmail.com>
2019-07-15 16:09:27 +0530

    Add project files.

---
+++ SwaggerDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private List<Employee> employeeList;
+        public EmployeeController()
+        {
+            employeeList = new List<Employee>
+            {
+                new Employee{Id = 1, FirstName="Jane", LastName="Doe", Age = 21, Title = "Developer"},
+                new Employee{Id = 2, FirstName="Bob", LastName="Martin", Age = 21, Title = "Architect"}
+            };
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Employee>), 200)]
+        public ActionResult Get()
+        {
+            return Ok(employeeList);
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        [ProducesResponseType(typeof(Employee), 200)]
+        [ProducesResponseType(typeof(ErrorResponse), 400)]
+        public ActionResult Get(int id)
+        {
+            var employee = employeeList.Where(x => x.Id == id).FirstOrDefault();
+            if (employee == null)
+            {
+                return BadRequest(new ErrorResponse { ErrorCode = 404, ErrorMessage = "Employee Not Found" });
+            }
+            return Ok(employee);
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
